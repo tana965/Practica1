7ea0c0b97e2adfbbeb5fd2cf692cbb16ef62bf83
tana965 <2dana2015@gmail.com>
2023-09-06 07:55:03 +0300

    практика 1

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,60 @@
+fun main(args: Array<String>) {
+// 1 номер
+    val exercises: Int = 13
+    var exercisesSolved: Int = 0
+    exercisesSolved +=1
+//////////
+// 2 номер
+    val numDva: Int = 18
+/////////
+// 3 номер
+    var numTri: Double = 18.0
+    numTri = (numTri + 30)/2
+////////
+// 4 номер
+    val testNumber: Int = 15
+    val evenOdd: Int = testNumber%2
+//evenOdd не выдал ошибку
+///////
+// 5 номер
+    var answer: Int = 0
+    answer +=1
+    answer +=10
+    answer= answer shr 3
+    println(answer)
+// результат 1
+//////
+// 6 номер
+    age = 16
+    print(age)
+    age = 30
+    print(age)
+// var
+/////
+// 7 номер
+    val a: Int = 46
+    val b: Int = 10
+    val answer1: Int = (a*100)+b
+    val answer2: Int = (a*100)+(b*100)
+    val answer3: Int = (a*100)+(b/10)
+// answer1 4610
+//answer2 5600
+//answer3 4601
+/////
+// 8 номер
+    (5*3)-(4/2*2)
+/////
+// 9 номер
+    val a: Double = 3.0
+    val b: Double = 5.0
+    val average: Double = (a+b)/2
+/////
+// 10 номер
+    val fahrenheit: Double = 12.0
+    val celcius: Double = 1.8*(fahrenheit-32)
+/////
+// 12 номер
+    val degrees: Double = 90.0
+    val radians: Double =( degrees * Math.PI)/180
+/////
+}
